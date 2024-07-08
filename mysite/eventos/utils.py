ec611709e2948d12d21161b720a2601dc07b19c4
pedrin-pedrada <pedropschneider@gmail.com>
2024-07-08 11:57:40 -0300

    HTML email

--- mysite/eventos/utils.py
+++ mysite/eventos/utils.py
@@ -1,6 +1,6 @@
     
-from django.core.mail import send_mail
 from mailersend import emails
+from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 
 def send_mailer_send_email(to_emails, subject, content):
@@ -19,9 +19,22 @@ def send_mailer_send_email(to_emails, subject, content):
     response = mailer.send(email_params)
     return response
 
-def send_test_email(to_emails,subject,content):
+def send_test_email(to_emails, subject, content):
     print(to_emails)
     email_from = settings.EMAIL_HOST_USER
-    to_emails = ['eoviezzer@ucs.br','ppschneider@ucs.br','vhsantos@ucs.br']
+    to_emails = ['eoviezzer@ucs.br', 'ppschneider@ucs.br', 'vhsantos@ucs.br']
+
     for email in to_emails:
-        send_mail(subject, content, email_from, [email])
+        # Criar o email
+        email_message = EmailMultiAlternatives(
+            subject=subject,
+            body=content,  # Texto sem formatação (plain text)
+            from_email=email_from,
+            to=[email]
+        )
+
+        # Anexar a versão HTML do email
+        email_message.attach_alternative(content, "text/html")
+
+        # Enviar o email
+        email_message.send()
